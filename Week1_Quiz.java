2c22c478ba2bdf4b573b34f1e4d9075f98ac91ff
Michelle Arcelia Neptune <50461978+MichelleArcelia@users.noreply.github.com>
2020-11-05 18:21:11 -0600

    Update Week1_Quiz.java

--- Week1_Quiz.java
+++ Week1_Quiz.java
@@ -1,1 +1,16 @@
+Basic Data Structures Quiz
 
+Which of the basic data structures is the most suitable if you need to access its elements by their positions in O(1)O(1)O(1) time (this is called random access)?
+ARRAY
+
+Which of the basic data structures is the most suitable if you want to be able to insert elements in the middle in O(1)O(1)O(1)?
+LIST
+
+Which of the basic data structures is the most suitable if you only need to insert the elements in the back and to extract elements from the front?
+QUEUE
+
+Which of the basic data structures is the most suitable if you only need to implement recursion in a programming language? When you make a recursive call, you need to save the function you are currently in and its parameters values in some data structure, so that when you go out of the recursion you can restore the state. When you go out of the recursive call, you will always need to extract the last element that was put in the data structure.
+STACK
+
+Which of the basic data structures is the most suitable if you need to store the directory structure on your hard drive?
+TREE
